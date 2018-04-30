089b272d27a506952ee4a91251f00c88b04e1b1d
Cyril Antoni <cyril.lay.antoni@gmail.com>
2018-04-30 11:35:18 +0200

    add search field

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -11,12 +12,16 @@ class App extends Component {
       searchfield: ''
     }
   }
+  onSearchChange = event => {
+    this.setState({ searchfield: event.target.value });
+  }
   render() {
+    const filteredRobots = this.state.robots.filter(robot => robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase()));
     return (
       <div className='tc'>
-        <h1>RoboFriend</h1>
-        <SearchBox />
-        <CardList robots={this.state.robots} />
+        <h1 className='f1'>RoboFriend</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots} />
       </div>
     );
   }
--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Card = ( {id, name, email} ) => {
+const Card = ( {key, id, name, email} ) => {
   return (
-    <div className="tc bg-light-green dib br3 pa3 ma2 grow shadow-5">
+    <div key={key} className="tc bg-light-green dib br3 pa3 ma2 grow shadow-5">
       <img src={`https://robohash.org/${id}?200x200`} alt="robot-{id}" />
       <div>
         <h2>{name}</h2>
