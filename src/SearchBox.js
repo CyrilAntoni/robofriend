010cc92aa299a35501bc648fe9db80095f73e42e
Cyril Antoni <cyril.lay.antoni@gmail.com>
2018-04-30 12:16:22 +0200

    add Scroll component

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchBox = ({searchfield, searchChange}) => {
   return (
     <input
-      className='pa3 ba b--green bg-lightest-blue'
+      className='pa3 mb4 ba b--green bg-lightest-blue'
       type='search'
       placeholder='search robots'
       onChange={searchChange}
